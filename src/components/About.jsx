ffa98ca3ab52fcc1b3d3d127afd241463e1bc3b1
Amy Kim <90531819+kimmy190@users.noreply.github.com>
2025-12-19 11:33:41 -0500

    Revise About section content for clarity and updates
    Updated the About section to reflect recent changes in education and interests.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,8 +20,7 @@ const About = ({ onClose }) => {
             {/* content under the upper bar */}
                 <div className="justify-center modal-content">
                 {/* <h2 className='font-bold'>About me!</h2> */}
-                <p className="modal-writing">Hi! I&apos;m Amy, a 4th year computer science and a statistic student at the University of Toronto. Also a passionate developer fascinated on bringing my ideas to life through various applications. 
-                <br/>I&apos;m always eager to try new things, and learn from each experiences.
+                <p className="modal-writing">Hello! I’m Amy, a recent Computer Science graduate from the University of Toronto who thrives on learning through experimentation. With the passion to build thoughtful software solutions, I’m highly motivated to take on new challenges, test ideas, and continuously improve through hands-on experience!
                 <br/><br/>
                 + while I'm not coding I love doing arts as a side to fuel my creativity 
                 
